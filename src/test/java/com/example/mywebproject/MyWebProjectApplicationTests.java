cb9d50ea6a1c55668d45f62c64c9384ea9b611b4
A2571 <2571469016@qq.com>
2025-07-20 05:38:06 +0800

    我的web项目

--- src/test/java/com/example/mywebproject/MyWebProjectApplicationTests.java
+++ src/test/java/com/example/mywebproject/MyWebProjectApplicationTests.java
@@ -1,17 +1,9 @@
 package com.example.mywebproject;
-import com.example.mywebproject.Pojo.myResult;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-
-import java.security.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Timer;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 
 @SpringBootTest
